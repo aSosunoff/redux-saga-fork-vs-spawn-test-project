1dd83e74368f9b11edaa069c0bcf3f5e3473518c
asosunoff <al.sosunoff@gmail.com>
2021-11-12 19:08:53 +0300

    fix

--- src/feature/people/saga/watchAddPeople.ts
+++ src/feature/people/saga/watchAddPeople.ts
@@ -1,52 +1,17 @@
-import { take, put, select } from "redux-saga/effects";
-import { StateRoot } from "../../../app/redux/reducers";
-import { getIdFromUrl } from "../../../getIdFromUrl";
-import {
-  ActionPeoleAdd,
-  ActionPeoleDelete,
-  ActionPeopleSuccess,
-  PEOPLE_ADD,
-  PEOPLE_DELETE,
-  PEOPLE_SUCCESS,
-} from "../action";
-import { selectorPeople } from "../selectors/selector-people";
-
-/* function* addPerson({ payload }: ActionPeoleAdd) {
-  console.log(payload);
-} */
+import { take, call, fork } from "redux-saga/effects";
+import { Person } from "../../../app/interfaces/person";
+import { ActionPeoleAdd, PEOPLE_ADD } from "../action";
+import { addPerson } from "./add-person";
+import { deletePerson } from "./delete-person";
 
 export function* watchAddPeople() {
   while (true) {
-    /* ADD */
-    const { payload: newPerson }: ActionPeoleAdd = yield take<ActionPeoleAdd>(PEOPLE_ADD);
+    const { payload }: ActionPeoleAdd = yield take<ActionPeoleAdd>(PEOPLE_ADD);
 
-    const { pageData: addPageData }: StateRoot["people"] = yield select(selectorPeople);
+    const person: Person | void = yield call(addPerson, payload);
 
-    if (addPageData) {
-      yield put<ActionPeopleSuccess>({
-        type: PEOPLE_SUCCESS,
-        payload: {
-          ...addPageData,
-          results: [...addPageData.results, newPerson],
-        },
-      });
-    }
-
-    /* DELETE */
-    yield take<ActionPeoleDelete>(PEOPLE_DELETE(newPerson.url));
-
-    const { pageData: deletePageData }: StateRoot["people"] = yield select(selectorPeople);
-
-    if (deletePageData) {
-      yield put<ActionPeopleSuccess>({
-        type: PEOPLE_SUCCESS,
-        payload: {
-          ...deletePageData,
-          results: deletePageData.results.filter(
-            ({ url }) => getIdFromUrl(url) !== getIdFromUrl(newPerson.url)
-          ),
-        },
-      });
+    if (person) {
+      yield fork(deletePerson, person.url);
     }
   }
 }
