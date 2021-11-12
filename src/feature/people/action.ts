8018c6d68b80e492c45aeb75521b5172e36b9cf6
asosunoff <al.sosunoff@gmail.com>
2021-11-12 14:11:49 +0300

    add

---
+++ src/feature/people/action.ts
@@ -0,0 +1,28 @@
+import { PagesType } from "../../app/interfaces/page-type";
+import { Person } from "../../app/interfaces/person";
+import { CreateAction } from "../../app/redux/reducers/helpers";
+
+export const PEOPLE_REQUEST = "PEOPLE_REQUEST";
+export type dispatchPeopleRequest = typeof PEOPLE_REQUEST;
+export type ActionPeopleRequest = CreateAction<ActionPeople, dispatchPeopleRequest>;
+
+export const PEOPLE_SUCCESS = "PEOPLE_SUCCESS";
+export type dispatchPeopleSuccess = typeof PEOPLE_SUCCESS;
+export type ActionPeopleSuccess = CreateAction<ActionPeople, dispatchPeopleSuccess>;
+
+export const PEOPLE_FAILURE = "PEOPLE_FAILURE";
+export type dispatchPeopleFailure = typeof PEOPLE_FAILURE;
+export type ActionPeoleFailure = CreateAction<ActionPeople, dispatchPeopleFailure>;
+
+export type ActionPeople = {
+  [PEOPLE_REQUEST]: {
+    payload: {
+      page: number;
+      search: string;
+    };
+  };
+
+  [PEOPLE_SUCCESS]: { payload: PagesType<Person> };
+
+  [PEOPLE_FAILURE]: { payload: any };
+};
