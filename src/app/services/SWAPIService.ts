012d3c1468f7b718e5836a50d58adc565f755b3b
asosunoff <al.sosunoff@gmail.com>
2021-11-12 14:16:50 +0300

    fix

--- src/app/services/SWAPIService.ts
+++ src/app/services/SWAPIService.ts
@@ -1,28 +1,68 @@
+import { PagesType } from "../interfaces/page-type";
 import { Person } from "../interfaces/person";
 import { Planet } from "../interfaces/planet";
 
-const getData = async <Result>(type: string, init?: RequestInit) => {
-  const request = await fetch(`https://swapi.dev/api/${type}/`, init);
+const getParams = (page: number, search: string = "") => {
+  let searchQuery = "";
+
+  if (search !== "") {
+    searchQuery += `search=${search}`;
+  }
+
+  let pageQuery = "";
+
+  if (page !== 1) {
+    pageQuery = `page=${page}`;
+  }
+
+  const searchParams = [searchQuery, pageQuery].filter(Boolean);
+
+  return `${searchParams.length ? "?" : ""}${searchParams.join("&")}`;
+};
+
+const request = async <Result>(url: string, init?: RequestInit) => {
+  const request = await fetch(url, init);
+
+  if (!request.ok) {
+    const body = await request.json();
+    throw body;
+  }
 
   const data = await request.json();
 
   return data as Result;
 };
 
-export class SWAPIService {
-  static getPersons = async (init?: RequestInit) => {
-    const { results } = await getData<{
-      results: Person[];
-    }>("people", init);
+const BASE_URL = "https://swapi.dev/api";
 
-    return results;
+export class SWAPIService {
+  static getPeople = async (page: number, search: string = "", init?: RequestInit) => {
+    const data = await request<PagesType<Person>>(
+      `${BASE_URL}/people/${getParams(page, search)}`,
+      init
+    );
+
+    return data;
   };
 
-  static getPlanets = async (init?: RequestInit) => {
-    const { results } = await getData<{
-      results: Planet[];
-    }>("planets", init);
+  static getPeopleDetail = async (id: string, init?: RequestInit) => {
+    const data = await request<Person>(`${BASE_URL}/people/${id}`, init);
 
-    return results;
+    return data;
+  };
+
+  static getPlanets = async (page: number, search: string = "", init?: RequestInit) => {
+    const data = await request<PagesType<Planet>>(
+      `${BASE_URL}/planets/${getParams(page, search)}`,
+      init
+    );
+
+    return data;
+  };
+
+  static getPlanetDetail = async (id: string, init?: RequestInit) => {
+    const data = await request<Planet>(`${BASE_URL}/planets/${id}`, init);
+
+    return data;
   };
 }
