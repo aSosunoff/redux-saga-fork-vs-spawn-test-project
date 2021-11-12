689c0cb169b395b346d35671a4790e0bfbd97e4f
asosunoff <al.sosunoff@gmail.com>
2021-11-12 19:08:49 +0300

    fix

--- src/feature/people/saga/loadPeopleList.ts
+++ src/feature/people/saga/loadPeopleList.ts
@@ -1,4 +1,4 @@
-import { put, call } from "redux-saga/effects";
+import { put, call, fork } from "redux-saga/effects";
 import { PagesType } from "../../../app/interfaces/page-type";
 import { Person } from "../../../app/interfaces/person";
 import { SWAPIService } from "../../../app/services/SWAPIService";
@@ -9,6 +9,7 @@ import {
   PEOPLE_FAILURE,
   PEOPLE_SUCCESS,
 } from "../action";
+import { deletePerson } from "./delete-person";
 
 export function* loadPeopleList({ payload: { page, search } }: ActionPeopleRequest) {
   try {
@@ -18,6 +19,10 @@ export function* loadPeopleList({ payload: { page, search } }: ActionPeopleReque
       type: PEOPLE_SUCCESS,
       payload: pageData,
     });
+
+    for (const person of pageData.results) {
+      yield fork(deletePerson, person.url);
+    }
   } catch (error: any) {
     yield put<ActionPeoleFailure>({
       type: PEOPLE_FAILURE,
