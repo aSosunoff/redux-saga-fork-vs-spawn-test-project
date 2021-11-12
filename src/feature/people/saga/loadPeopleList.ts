ba26bad4d01b4b9cc5b4584980457c4433f9ecfe
asosunoff <al.sosunoff@gmail.com>
2021-11-12 15:14:48 +0300

    fix

--- src/feature/people/saga/loadPeopleList.ts
+++ src/feature/people/saga/loadPeopleList.ts
@@ -1,15 +1,27 @@
 import { put, call } from "redux-saga/effects";
 import { PagesType } from "../../../app/interfaces/page-type";
 import { Person } from "../../../app/interfaces/person";
-import { ActionSetPeople } from "../../../app/redux/reducers/peopleReducer";
 import { SWAPIService } from "../../../app/services/SWAPIService";
-import { ActionPeopleRequest } from "../action";
+import {
+  ActionPeoleFailure,
+  ActionPeopleRequest,
+  ActionPeopleSuccess,
+  PEOPLE_FAILURE,
+  PEOPLE_SUCCESS,
+} from "../action";
 
 export function* loadPeopleList({ payload: { page, search } }: ActionPeopleRequest) {
-  const payload: PagesType<Person> = yield call(SWAPIService.getPeople, page, search);
+  try {
+    const pageData: PagesType<Person> = yield call(SWAPIService.getPeople, page, search);
 
-  yield put<ActionSetPeople>({
-    type: "SET_PEOPLE",
-    payload,
-  });
+    yield put<ActionPeopleSuccess>({
+      type: PEOPLE_SUCCESS,
+      payload: pageData,
+    });
+  } catch (error: any) {
+    yield put<ActionPeoleFailure>({
+      type: PEOPLE_FAILURE,
+      payload: error,
+    });
+  }
 }
