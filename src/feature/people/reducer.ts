7a96ff5acf5c9fdd277e7ea3b10159c5aec9975e
asosunoff <al.sosunoff@gmail.com>
2021-11-12 15:28:58 +0300

    fix

--- src/feature/people/reducer.ts
+++ src/feature/people/reducer.ts
@@ -3,11 +3,13 @@ import { Reducer } from "redux";
 import { PagesType } from "../../app/interfaces/page-type";
 import { Person } from "../../app/interfaces/person";
 import { CreateReducer, Handler } from "../../app/redux/reducers/helpers";
+import { getIdFromUrl } from "../../getIdFromUrl";
 import {
   ActionPeoleFailure,
   ActionPeople,
   ActionPeopleRequest,
   ActionPeopleSuccess,
+  PEOPLE_DELETE,
   PEOPLE_FAILURE,
   PEOPLE_REQUEST,
   PEOPLE_SUCCESS,
@@ -40,16 +42,32 @@ const handlers: Handler<State, ActionPeople> = {
     loading: true,
     error: null,
   }),
+
   [PEOPLE_FAILURE]: (state, action) => ({
     ...state,
     loading: false,
     error: action.payload,
   }),
+
   [PEOPLE_SUCCESS]: (state, action) => ({
     ...state,
     loading: false,
     pageData: action.payload,
   }),
+
+  [PEOPLE_DELETE]: (state, action) =>
+    state.pageData
+      ? {
+          ...state,
+          pageData: {
+            ...state.pageData,
+            results:
+              state.pageData?.results.filter(
+                ({ url }) => getIdFromUrl(url) !== getIdFromUrl(action.payload.url)
+              ) ?? [],
+          },
+        }
+      : state,
 
   /* ADD_PEOPLE: (state, action) =>
     state.pageData
@@ -63,20 +81,6 @@ const handlers: Handler<State, ActionPeople> = {
       : {
           pageData: state.pageData,
         }, */
-
-  /* DELETE_PEOPLE: (state, action) =>
-    state.pageData
-      ? {
-          ...state,
-          pageData: {
-            ...state.pageData,
-            results:
-              state.pageData?.results.filter(
-                ({ url }) => getIdFromUrl(url) !== getIdFromUrl(action.payload.url)
-              ) ?? [],
-          },
-        }
-      : { pageData: state.pageData }, */
 
   DEFAULT: (state) => state,
 };
