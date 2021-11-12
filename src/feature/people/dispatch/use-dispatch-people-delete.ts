4378ae787e167fca8152409412b0d8195912215b
asosunoff <al.sosunoff@gmail.com>
2021-11-12 17:46:51 +0300

    fix

--- src/feature/people/dispatch/use-dispatch-people-delete.ts
+++ src/feature/people/dispatch/use-dispatch-people-delete.ts
@@ -1,16 +1,14 @@
 import { Dispatch, useCallback } from "react";
 import { useDispatch } from "react-redux";
-import { Person } from "../../../app/interfaces/person";
 import { ActionPeoleDelete, PEOPLE_DELETE } from "../action";
 
 export const useDispatchPeopleDelete = () => {
   const dispatch = useDispatch<Dispatch<ActionPeoleDelete>>();
 
   const dispatchPeopleDelete = useCallback(
-    (person: Person) => {
+    (id: string) => {
       dispatch({
-        type: PEOPLE_DELETE,
-        payload: person,
+        type: PEOPLE_DELETE(id),
       });
     },
     [dispatch]
