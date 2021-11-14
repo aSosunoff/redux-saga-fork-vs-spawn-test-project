c9307f7eae427c253eb21cbad1651845823d3c0c
asosunoff <al.sosunoff@gmail.com>
2021-11-14 17:38:55 +0300

    fix

--- src/feature/people/saga/index.ts
+++ src/feature/people/saga/index.ts
@@ -1,8 +1,7 @@
 import { all, fork } from "@redux-saga/core/effects";
-import { loadPeopleList } from "./loadPeopleList";
 import { watchAddPerson } from "./watchAddPerson";
 import { watchRequestPeople } from "./watchRequestPeople";
 
 export function* peopleRootSaga() {
-  yield all([fork(loadPeopleList), fork(watchAddPerson)]);
+  yield all([fork(watchRequestPeople), fork(watchAddPerson)]);
 }
