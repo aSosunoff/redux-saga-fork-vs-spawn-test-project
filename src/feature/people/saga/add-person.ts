4525e4b9dc70b57de3e9b69ced23f9be9eb93fa5
asosunoff <al.sosunoff@gmail.com>
2021-11-14 17:14:04 +0300

    add

--- src/feature/people/saga/add-person.ts
+++ src/feature/people/saga/add-person.ts
@@ -1,19 +1,16 @@
-import { put, select } from "redux-saga/effects";
+import { call, select } from "redux-saga/effects";
 import { Person } from "../../../app/interfaces/person";
 import { StateRoot } from "../../../app/redux/reducers";
-import { ActionPeopleSuccess, PEOPLE_SUCCESS } from "../action";
 import { selectorPeople } from "../selectors/selector-people";
+import { peopleSuccess } from "./helper-store/store/peopleSuccess";
 
 export function* addPerson(person: Person) {
   const { pageData }: StateRoot["people"] = yield select(selectorPeople);
 
   if (pageData) {
-    yield put<ActionPeopleSuccess>({
-      type: PEOPLE_SUCCESS,
-      payload: {
-        ...pageData,
-        results: [...pageData.results, person],
-      },
+    yield call(peopleSuccess, {
+      ...pageData,
+      results: [...pageData.results, person],
     });
 
     return person;
