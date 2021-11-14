92821633df2c0e038e56b80dc5a02c19807b1109
asosunoff <al.sosunoff@gmail.com>
2021-11-14 17:14:17 +0300

    fix

--- src/feature/people/saga/loadPeopleList.ts
+++ src/feature/people/saga/loadPeopleList.ts
@@ -1,32 +1,27 @@
-import { put, call, fork } from "redux-saga/effects";
+import { call, fork } from "redux-saga/effects";
 import { PagesType } from "../../../app/interfaces/page-type";
 import { Person } from "../../../app/interfaces/person";
 import { SWAPIService } from "../../../app/services/SWAPIService";
-import {
-  ActionPeoleFailure,
-  ActionPeopleRequest,
-  ActionPeopleSuccess,
-  PEOPLE_FAILURE,
-  PEOPLE_SUCCESS,
-} from "../action";
+import { ActionPeopleRequest } from "../action";
 import { deletePerson } from "./delete-person";
+import { peopleFailure } from "./helper-store/store/peopleFailure";
+import { peopleRequest } from "./helper-store/store/peopleRequest";
+import { peopleSuccess } from "./helper-store/store/peopleSuccess";
 
-export function* loadPeopleList({ payload: { page, search } }: ActionPeopleRequest) {
-  try {
-    const pageData: PagesType<Person> = yield call(SWAPIService.getPeople, page, search);
+export function* loadPeopleList() {
+  while (true) {
+    try {
+      const { page, search }: ActionPeopleRequest["payload"] = yield call(peopleRequest);
 
-    yield put<ActionPeopleSuccess>({
-      type: PEOPLE_SUCCESS,
-      payload: pageData,
-    });
+      const pageData: PagesType<Person> = yield call(SWAPIService.getPeople, page, search);
 
-    for (const person of pageData.results) {
-      yield fork(deletePerson, person.url);
+      yield call(peopleSuccess, pageData);
+
+      for (const person of pageData.results) {
+        yield fork(deletePerson, person.url);
+      }
+    } catch (error: any) {
+      yield call(peopleFailure, error);
     }
-  } catch (error: any) {
-    yield put<ActionPeoleFailure>({
-      type: PEOPLE_FAILURE,
-      payload: error,
-    });
   }
 }
