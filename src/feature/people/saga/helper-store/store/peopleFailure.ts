450ef48cf221e189ef779ffa8191d2a0af6d4375
asosunoff <al.sosunoff@gmail.com>
2021-11-14 18:03:30 +0300

    fix

--- src/feature/people/saga/helper-store/store/peopleFailure.ts
+++
@@ -1,9 +0,0 @@
-import { put } from "redux-saga/effects";
-import { ActionPeoleFailure, PEOPLE_FAILURE } from "../../../action";
-
-export function* peopleFailure(error: any) {
-  yield put<ActionPeoleFailure>({
-    type: PEOPLE_FAILURE,
-    payload: error,
-  });
-}
