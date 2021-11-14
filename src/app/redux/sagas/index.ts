1a38c71f9ac724f993c1e5a5ce00d5a58e1a2353
asosunoff <al.sosunoff@gmail.com>
2021-11-14 17:47:35 +0300

    fix

--- src/app/redux/sagas/index.ts
+++ src/app/redux/sagas/index.ts
@@ -1,24 +1,10 @@
-import { spawn, fork, call, all } from "redux-saga/effects";
+import { all } from "redux-saga/effects";
 import { errorRootSaga } from "../../../feature/error/error-root-saga";
 import { peopleRootSaga } from "../../../feature/people/saga";
+import { getRetrySagas } from "./effectHelper/get-retry-sagas";
 
 export function* rootSaga() {
-  const sagas = [peopleRootSaga, errorRootSaga];
-
-  const retrySagas = sagas.map((saga) =>
-    spawn(function* () {
-      while (true) {
-        try {
-          yield call(saga);
-          break;
-        } catch (error: any) {
-          console.log(error);
-        }
-      }
-    })
-  );
-
-  yield all(retrySagas);
+  yield all(getRetrySagas(peopleRootSaga, errorRootSaga));
 }
 
 /* export function* rootSaga() {
