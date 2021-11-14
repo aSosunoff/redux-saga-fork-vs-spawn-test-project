281aa418176ac24875e7f6b5ad77fc528f243b1b
asosunoff <al.sosunoff@gmail.com>
2021-11-14 17:14:31 +0300

    fix

---
+++ src/feature/people/saga/watchAddPerson.ts
@@ -0,0 +1,17 @@
+import { take, call, fork } from "redux-saga/effects";
+import { Person } from "../../../app/interfaces/person";
+import { ActionPeoleAdd, PEOPLE_ADD } from "../action";
+import { addPerson } from "./add-person";
+import { deletePerson } from "./delete-person";
+
+export function* watchAddPerson() {
+  while (true) {
+    const { payload }: ActionPeoleAdd = yield take<ActionPeoleAdd>(PEOPLE_ADD);
+
+    const person: Person | void = yield call(addPerson, payload);
+
+    if (person) {
+      yield fork(deletePerson, person.url);
+    }
+  }
+}
