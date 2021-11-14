2facafccff812c4ee645017fa36ba4f1249c9e36
asosunoff <al.sosunoff@gmail.com>
2021-11-14 18:03:15 +0300

    fix

---
+++ src/feature/people/saga/helper/delete-person.ts
@@ -0,0 +1,19 @@
+import { take, call, select } from "redux-saga/effects";
+import { StateRoot } from "../../../../app/redux/reducers";
+import { getIdFromUrl } from "../../../../getIdFromUrl";
+import { ActionPeoleDelete, PEOPLE_DELETE } from "../../action";
+import { selectorPeople } from "../../selectors/selector-people";
+import { peopleSuccess } from "../helper/store/peopleSuccess";
+
+export function* deletePerson(id: string) {
+  yield take<ActionPeoleDelete>(PEOPLE_DELETE(id));
+
+  const { pageData }: StateRoot["people"] = yield select(selectorPeople);
+
+  if (pageData) {
+    yield call(peopleSuccess, {
+      ...pageData,
+      results: pageData.results.filter(({ url }) => getIdFromUrl(url) !== getIdFromUrl(id)),
+    });
+  }
+}
